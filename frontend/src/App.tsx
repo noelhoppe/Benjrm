78848f9e4fed8e543db32e6898d31f9b7f346135
Noel Hoppe <noel.hoppe@gmx.de>
2026-05-08 16:10:23 +0200

    Setup Frontend with necessary dependencies React + TypeScript React Router Tanstack Query TailwindCSS shadcn/ui

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/shadcn/components/ui/button";
+
+function App() {
+    return (
+        <>
+            <h1 className="text-red-600">
+                Hello React + TypeScript + React Router + TanStack Query + TailwindCSS + shadcn/ui!
+            </h1>
+            <Button variant="default">Click me</Button>
+        </>
+    )
+}
+
+export default App
