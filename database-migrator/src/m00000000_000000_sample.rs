60a07352b1496dddbbfa44255b428a7908f21f7b
Untbu <mail@untbu.de>
2026-05-09 23:36:39 +0200

    fix sample migration

--- database-migrator/src/m00000000_000000_sample.rs
+++ database-migrator/src/m00000000_000000_sample.rs
@@ -23,7 +23,7 @@ impl MigrationTrait for Migration {
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         // Replace the sample below with your own migration scripts
         manager
-            .drop_table(Table::drop().table("table").to_owned())
+            .drop_table(Table::drop().table("example").to_owned())
             .await
     }
 }
