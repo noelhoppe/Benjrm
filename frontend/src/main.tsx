843033f9fe8545e8d00264bfbb452f66640d63ad
Noel Hoppe <noel.hoppe@gmx.de>
2026-05-20 18:35:58 +0200

    add QueryClient Provider (Context) with the specified queryClient to work with React / Tanstack Query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import ThemeProvider from "./context/ThemeProvider"
 import "./index.css"
 import App from "./App.tsx"
@@ -9,12 +10,16 @@ const container = document.getElementById("root")
 if (!container) throw new Error("Failed to find the root element")
 const root = createRoot(container)
 
+const queryClient = new QueryClient()
+
 root.render(
     <StrictMode>
-        <BrowserRouter>
-            <ThemeProvider defaultTheme="auto" storageKey="theme">
-                <App />
-            </ThemeProvider>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <ThemeProvider defaultTheme="auto" storageKey="theme">
+                    <App />
+                </ThemeProvider>
+            </BrowserRouter>
+        </QueryClientProvider>
     </StrictMode>
 )
