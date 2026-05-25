403c0365d3bc9f6eb5394a3debf97d88d98f5826
Noel Hoppe <noel.hoppe@gmx.de>
2026-05-25 15:53:19 +0200

    fix(api): add useWebSocketContext to the package's public api

--- frontend/src/api/websocket/index.ts
+++ frontend/src/api/websocket/index.ts
@@ -4,10 +4,12 @@
  */
 import useWebSocket from "@/api/websocket/hooks/useWebSocket.ts"
 import useSocketEvent from "@/api/websocket/hooks/useSocketEvent.ts"
+import useWebSocketContext from "@/api/websocket/hooks/useWebSocketContext.ts"
 
-export { useWebSocket, useSocketEvent }
+export { useWebSocket, useSocketEvent, useWebSocketContext }
 
 // How to import:
 // import { useWebSocket } from "@/api/websocket"
 // import { useSocketEvent } from "@/api/websocket"
-// import { useWebSocket, useSocketEvent } from "@/api/websocket"
+// import { useWebSocketContext } from "@/api/websocket"
+// import { useWebSocket, useSocketEvent, usewebSocketContext } from "@/api/websocket"
