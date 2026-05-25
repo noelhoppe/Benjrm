7841275b1bf3aaa1a28895907b232fc00427cf2e
Noel Hoppe <noel.hoppe@gmx.de>
2026-05-25 12:29:55 +0200

    fix(type): change timing type from Date to string (serialized representation)

--- frontend/src/api/websocket/types/serverEventHandler.ts
+++ frontend/src/api/websocket/types/serverEventHandler.ts
@@ -2,5 +2,5 @@ import type { ServerEvents } from "@/api/websocket/types/serverEvents.ts"
 
 export type ServerEventHandler<K extends keyof ServerEvents> = (
     payload: ServerEvents[K],
-    timing: Date
+    timing: string
 ) => void
