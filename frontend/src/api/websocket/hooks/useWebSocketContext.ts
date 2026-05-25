5dea06f24de4ec6e2ad81628356edd0e84039675
Noel Hoppe <noel.hoppe@gmx.de>
2026-05-25 15:24:44 +0200

    doc(useWebSocketContext): add tsdoc

--- frontend/src/api/websocket/hooks/useWebSocketContext.ts
+++ frontend/src/api/websocket/hooks/useWebSocketContext.ts
@@ -2,6 +2,12 @@ import { useContext } from "react"
 import WebSocketContext from "@/api/websocket/context/WebSocketContext.ts"
 import type { WebsocketService } from "@/api/websocket/service/websocketService.ts"
 
+/**
+ * Custom hook to access the WebSocketContext, which provides the WebsocketService instance for managing WebSocket connections
+ * and sending and receiving typed events in a bidirectional way.
+ * @returns WebsocketService instance of the WebSocketContext, which provides the WebsocketService instance for managing WebSocket connections and sending and receiving typed events in a bidirectional way.
+ * @throws Error if the hook is used outside a WebSocketContext provider.
+ */
 export default function useWebSocketContext(): WebsocketService {
     const websocketContext = useContext(WebSocketContext)
     if (!websocketContext) {
