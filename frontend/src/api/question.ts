df647bbbcf549f96fcc6b5f825a073595430dc10
Noel Hoppe <noel.hoppe@gmx.de>
2026-05-26 18:08:34 +0200

    feat(api): adding adapter for api

--- frontend/src/api/question.ts
+++
@@ -1,5 +0,0 @@
-import { apiDelete } from "@/api/client.ts"
-
-export default async function deleteQuestion(quizId: string, questionId: string): Promise<void> {
-    return apiDelete(`quizzes/${quizId}/questions/${questionId}`)
-}
