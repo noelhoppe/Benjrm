d843d18ad5d8a46b2786284d264bde03e551fef1
Noel Hoppe <noel.hoppe@gmx.de>
2026-05-26 17:55:40 +0200

    feat(types): barrel file exporting package's public hooks

---
+++ frontend/src/api/questions/index.ts
@@ -0,0 +1,4 @@
+export { default as useCreateQuestion } from "@/api/questions/hooks/useCreateQuestion"
+export { default as useQuestions } from "@/api/questions/hooks/useQuestions"
+export { default as useUpdateQuestion } from "@/api/questions/hooks/useUpdateQuestion"
+export { default as useDeleteQuestion } from "@/api/questions/hooks/useDeleteQuestion"
