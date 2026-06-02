10a7047fd9cd0ca420ce79ee85e31b14077fec7f
Noel Hoppe <noel.hoppe@gmx.de>
2026-06-02 21:44:39 +0200

    chore(schemathesis hooks): called before each test case

--- schemathesis/src/hooks/normalize_bools.py
+++ schemathesis/src/hooks/normalize_bools.py
@@ -7,6 +7,7 @@ def normalize_bools(ctx, case: Case, kwargs):
     generated_query_parameters = case.query
     for key, value in generated_query_parameters.items():
         if isinstance(value, bool):
+            print(value)
             generated_query_parameters[key] = "true" if value else "false"
     case.query = generated_query_parameters
 
