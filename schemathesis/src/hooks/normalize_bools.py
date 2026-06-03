a4f3601a35e067650ae18205daeb6002631c5238
Noel Hoppe <noel.hoppe@gmx.de>
2026-06-03 16:29:53 +0200

    refactor(hooks): remove debug print

--- schemathesis/src/hooks/normalize_bools.py
+++ schemathesis/src/hooks/normalize_bools.py
@@ -3,11 +3,9 @@ from schemathesis import Case
 
 
 @schemathesis.hook("before_call")
-def normalize_bools(ctx, case: Case, kwargs):
+def normalize_bools(ctx, case: Case, **kwargs):
     generated_query_parameters = case.query
     for key, value in generated_query_parameters.items():
         if isinstance(value, bool):
-            print(value)
             generated_query_parameters[key] = "true" if value else "false"
     case.query = generated_query_parameters
-
