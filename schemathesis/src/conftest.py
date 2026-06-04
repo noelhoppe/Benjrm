55995dff5e08cd343d2586ddaa3e6107710055a6
Noel Hoppe <noel.hoppe@gmx.de>
2026-06-04 15:00:52 +0200

    chore(deps): pytest and fixtures no longer needed

--- schemathesis/src/conftest.py
+++
@@ -1,4 +0,0 @@
-from fixtures.questions import questions
-from fixtures.quizzes import quizzes
-from fixtures.auth_client import auth_client
-from fixtures.client import client
--- schemathesis/src/fixtures/auth_client.py
+++
@@ -1,8 +0,0 @@
-# import pytest
-#
-# from fixtures.client import Client
-#
-# @pytest.fixture()
-# def auth_client(client: Client) -> Client:
-#     client.get("/auth/login/dummy/1")
-#     return client
