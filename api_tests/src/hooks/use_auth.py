f2a94d09ceb30acc1a9c2fd344f876368274015f
Noel Hoppe <noel.hoppe@gmx.de>
2026-06-09 17:10:59 +0200

    fix(caching): cache auth cookie

--- api_tests/src/hooks/use_auth.py
+++ api_tests/src/hooks/use_auth.py
@@ -2,8 +2,12 @@ import schemathesis
 from schemathesis import Case
 import requests
 
+cookie = {}
+
 @schemathesis.hook("before_call")
 def use_auth(ctx, case: Case, **kwargs):
-    dummy_authentication_endpoint_debug_builds = "http://localhost:8080/auth/login/dummy/2"
-    cookie = requests.get(dummy_authentication_endpoint_debug_builds).cookies.get_dict()
+    global cookie
+    if not cookie:
+        dummy_authentication_endpoint_debug_builds = "http://localhost:8080/auth/login/dummy/2"
+        cookie = requests.get(dummy_authentication_endpoint_debug_builds).cookies.get_dict()
     case.headers["Cookie"] = "; ".join([f"{key}={value}" for key, value in cookie.items()])
